8386c367148f9f7061f82f736fbeec77cb3ec11e
Alexander Repeta <alexander.repeta@gmail.com>
2025-08-15 13:13:12 +0300

    Add examples

--- src/pages/UserDetailsPage.jsx
+++ src/pages/UserDetailsPage.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link, Outlet, useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 
 export default function UserDetailsPage() {
+  const location = useLocation();
+  const backLinkRef = useRef(location.state ?? "/dashboard");
+
   const { userId } = useParams();
   const [user, setUser] = useState(null);
 
@@ -17,7 +20,9 @@ export default function UserDetailsPage() {
 
   return (
     <div>
-      <h1>UserDetailsPage</h1>
+      <h1>
+        UserDetailsPage <Link to={backLinkRef.current}>Back to dashboard</Link>
+      </h1>
       {user && (
         <>
           <p>First name: {user.firstName}</p>
